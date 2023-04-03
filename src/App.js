e0b881fb114d39b5e7a68b0147e27d3013b63892
Pramod Kale <104553634+Pramod-kale@users.noreply.github.com>
2023-04-03 11:33:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src='http://picsum.photos/500' style={{ borderRadius: '50%' }} className="App-logo" alt="logo" />
         <p>
-          this is for the testing purpose
+          this is for the testing 
         </p>
       </header>
     </div>
